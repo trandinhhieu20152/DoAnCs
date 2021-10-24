bbc326bdc9c23499e503df9830b45d3c6a722208
thanh888 <nguyentuthanh032002@gmail.com>
2021-10-24 21:45:28 +0700

    thanh

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,6 @@ use Illuminate\Support\Facades\Route;
 /* Front-end */
 Route::get('/','HomeController@index');
 
-
-
 /* Back-end */
 Route::get('/admin','AdminController@index');
 Route::get('/dashboard','AdminController@show_dashboard');
@@ -26,10 +25,10 @@ Route::get('/logout','AdminController@log_out');
 Route::post('/admin-dashboard','AdminController@dashboard');
 
 // Category products
-// Route::get('/all-category-products','CategoryProduct@all_category_products');
-// Route::get('/add-category-products','CategoryProduct@add_category_products');
-// Route::get('/delete-category-product/{category_product_id}','CategoryProduct@delete_category_products');
+Route::get('/all-category-products','CategoryProduct@all_category_products');
+Route::get('/add-category-products','CategoryProduct@add_category_products');
+Route::get('/delete-category-product/{category_product_id}','CategoryProduct@delete_category_products');
 
 
-// Route::post('/save-category-product','CategoryProduct@save_category_products');
-// Route::post('/update-category-product/{category_product_id}','CategoryProduct@update_category_products');
+Route::post('/save-category-product','CategoryProduct@save_category_products');
+Route::post('/update-category-product/{category_product_id}','CategoryProduct@update_category_products');
