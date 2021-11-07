edbdc064d902b725105875eedc790f62b4bd2857
trandinhhieu20152 <trandinhhieu20152@gmail.com>
2021-11-07 21:30:01 +0700

    submit1.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -40,7 +40,7 @@
             </button>
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="{{URL::to('/home')}}" class="nav-link">Home</a></li>
+                    <li class="nav-item active"><a href="{{URL::to('/')}}" class="nav-link">Home</a></li>
                     <li class="nav-item"><a href="menu.html" class="nav-link">Menu</a></li>
                     <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
                     <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
